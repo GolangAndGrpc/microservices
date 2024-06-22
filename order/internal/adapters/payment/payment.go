2843912e83c0633ab789f4827bc2891966a9c7fa
Pruthviraj Suresh Hawale <prha0922@netcracker.com>
2024-06-22 15:00:04 +0530

    implement retyr

--- order/internal/adapters/payment/payment.go
+++ order/internal/adapters/payment/payment.go
@@ -7,7 +7,9 @@ import (
 
 	"github.com/GolangAndGrpc/microserviceApis/golang/payments"
 	"github.com/GolangAndGrpc/microservices/order/internal/application/core/domain"
+	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
@@ -18,6 +20,13 @@ type Adapter struct {
 
 func NewAdapter(paymentServiceUrl string) (*Adapter, error){
 	var opts []grpc.DialOption
+	opts = append(opts, 
+	grpc.WithUnaryInterceptor(grpc_retry.UnaryClientInterceptor(
+		grpc_retry.WithCodes(codes.Unavailable, codes.ResourceExhausted),
+		grpc_retry.WithMax(5),
+		grpc_retry.WithBackoff(grpc_retry.BackoffLinear(time.Second)),
+	)))
+
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	conn, err := grpc.NewClient("localhost:8088", opts...)
 	if err != nil {
